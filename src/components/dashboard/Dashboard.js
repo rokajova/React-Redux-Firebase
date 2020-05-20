2c66f41755f70eed3dd18d8e93a307ba746762fa
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-05-20 15:43:49 +0300

    24
    24

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
-import ProjectList from '../projects/ProjectList'
-import Notifications from './Notifications'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import ProjectList from "../projects/ProjectList";
+import Notifications from "./Notifications";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
   render() {
-    
     // console.log(this.props);
     const { projects } = this.props;
-    
+
     return (
       <div className="dashboard container">
         <div className="row">
@@ -20,14 +21,17 @@ class Dashboard extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    projects: state.project.projects
-  }
-}
+    projects: state.firestore.ordered.projects,
+  };
+};
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(Dashboard);
