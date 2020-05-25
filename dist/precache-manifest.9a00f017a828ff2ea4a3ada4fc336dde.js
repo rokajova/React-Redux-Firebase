e06504cd6bce92c1e94a0abc1c9d038b75d2e145
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-05-25 04:30:52 +0300

    End
    End

---
+++ dist/precache-manifest.9a00f017a828ff2ea4a3ada4fc336dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da94c90a156405903f975205a41f829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "791f45ad04078efaccf5",
+    "url": "/static/css/main.aed33d7b.chunk.css"
+  },
+  {
+    "revision": "f35c482262f62d86d253",
+    "url": "/static/js/2.5d26873a.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.5d26873a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791f45ad04078efaccf5",
+    "url": "/static/js/main.ce5bfd6e.chunk.js"
+  },
+  {
+    "revision": "985ce749b3808677cbfe",
+    "url": "/static/js/runtime-main.84a57d64.js"
+  }
+]);
